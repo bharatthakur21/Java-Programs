773e79a38fefd409d0c6c53f0e3a30c6548a36ac
bharatthakur21 <bharatdangi2002@gmail.com>
2022-03-31 19:08:36 +0530

    Arrayprint

---
+++ Printarray.java
@@ -0,0 +1,8 @@
+public class Printarray {
+    public static void main(String[] args) {
+        int number[]={2,7,8,9,2,2,2,7,7,8,9,9,9};
+        for (int i=0; i<=number.length; i++){
+            System.out.println(i);
+        }
+    }
+}
